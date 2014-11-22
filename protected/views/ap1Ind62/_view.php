c83e78e3858d95964d56cdd636604bc97f39b087
fernando.gil <fernando.gil@unadmexico.mx>
2014-11-21 19:57:14 -0600

    generado crud ap1Ind62

---
+++ protected/views/ap1Ind62/_view.php
@@ -0,0 +1,43 @@
+<?php
+/* @var $this Ap1Ind62Controller */
+/* @var $data Ap1Ind62 */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('periodo_id')); ?>:</b>
+	<?php echo CHtml::encode($data->periodo_id); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('rubro')); ?>:</b>
+	<?php echo CHtml::encode($data->rubro); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('valor')); ?>:</b>
+	<?php echo CHtml::encode($data->valor); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('fecha_reg')); ?>:</b>
+	<?php echo CHtml::encode($data->fecha_reg); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('fecha_mod')); ?>:</b>
+	<?php echo CHtml::encode($data->fecha_mod); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('user_reg')); ?>:</b>
+	<?php echo CHtml::encode($data->user_reg); ?>
+	<br />
+
+	<?php /*
+	<b><?php echo CHtml::encode($data->getAttributeLabel('user_mod')); ?>:</b>
+	<?php echo CHtml::encode($data->user_mod); ?>
+	<br />
+
+	*/ ?>
+
+</div>
